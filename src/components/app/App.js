db8ed21a43cd56381f0848ded6be77246e7273ec
Dmitry Ivanov <di.ivanov2018@gmail.com>
2023-03-25 23:11:15 +0300

    add component app-header with styles

---
+++ src/components/app/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppHeader from "../app-header/AppHeader";
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div className="page">
+                <AppHeader />
+            </div>
+        )
+    }
+};
